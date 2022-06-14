9659230c2fa3cf1bb426207820aa1df62d918577
Toby Carlo Cripps <tobycarlocripps@jeannettes-imac.home>
2022-06-14 16:28:02 +0100

    login & profile

--- src/router.js
+++ src/router.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const { restart } = require("nodemon");
 
 const router = express.Router();
 
 const mockUser = {
-    username: "authguy",
+    username: "liltobe",
     password: "mypassword",
     profile: {
         firstName: 'Toby',
@@ -16,12 +17,29 @@ const mockUser = {
 const secret = "secret12345"
 
 router.post("/login", (req, res) => {
-
+    if (req.body.username === mockUser.username && req.body.password === mockUser.password) {
+        res.json(jwt.sign({
+            username: mockUser.username
+        }, secret))
+    }
+    else {
+        res.status(400)
+        res.json({
+            error: "Invalid username and/or password"
+        })
+    }
 });
 
 router.get("/profile", (req, res) => {
-  
+    try {
+        const token = req.headers["authorisation"];
+        jwt.verify(token, secret);
+        res.status(200).json({
+            profile: mockUser.profile
+        });
+    } catch (err) {
+        res.status(401).json("Unauthorised");
+    }
 });
-
 
 module.exports = router;
